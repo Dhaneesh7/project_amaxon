7c877334e32c60f9e1573885461466591d14646d
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-04-17 14:58:53 +0530

    Update Store.js

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -18,6 +18,18 @@ export const removeFromCart = (id) => ({ type: REMOVE_FROM_CART, payload: id });
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
+/*const exists = state.items.find((i) => i.id === action.payload.id);
+      if (exists) {
+        return {
+          ...state,
+          items: state.items.map((item) =>
+            item.id === action.payload.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      }
+*/
       return { ...state, cart: [...state.cart, action.payload] }; 
     case REMOVE_FROM_CART:
       return { ...state, cart: state.cart.filter(item => item.id !== action.payload) }; 
