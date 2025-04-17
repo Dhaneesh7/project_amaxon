73010bbd37dd83fd5a9b5dd9de860c67220a55a5
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-04-17 15:30:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="Cart" element={<Cart />} />
         </Routes>
+  <Route path="/wishlist" element={<Wishlist />} />
       </Router>
     </AuthProvider>
     // </Provider>
